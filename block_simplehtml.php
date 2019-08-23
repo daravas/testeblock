678ec71a59861bac53301d7eb199a42aacb0694f
Dara Vasconcelos <dcsv@cin.ufpe.br>
2019-08-23 16:53:01 -0300

    atualizacao da classe block_simplehtml

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -4,6 +4,18 @@ class block_simplehtml extends block_base{ //unica coisa que pode mudar é a par
     public function init(){ // dar valor a toda variavel que precisa ser instanciada
         $this->title = get_string('simplehtml','block_simplehtml'); 
     }
+
+    public function get_content(){
+        if($this->content !== null){
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $this->content->text = 'The content of our simple HTML block';
+        $this->content->footer = 'Footer here';
+
+        return $this->content;
+    }
 }
 
 
