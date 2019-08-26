1b8c99cb464700f4a3b881cd67c477caa5e5fad2
Dara Vasconcelos <dcsv@cin.ufpe.br>
2019-08-26 16:34:25 -0300

    adc arquivo edit_form

--- lang/en/block_simplehtml.php
+++ lang/en/block_simplehtml.php
@@ -2,6 +2,7 @@
 
 $string['pluginname'] = 'Simple HTML block';
 $string['simplehtml'] = 'Simple HTML';
+$string['blockstring'] = 'Teste edit form';
 $string['simplehtml:addinstance'] = 'Add a new simple HTML block';
 $string['simplehtml:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
 
